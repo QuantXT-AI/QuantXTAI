8dc2d635d35b5a3c626588eb80801d9aadb7bb26
QuantXT-AI <maximus42421@mail.com>
2024-12-18 16:06:54 +0700

    chatbot enhance

--- src/app/(public)/cryaistal-agent/page.tsx
+++ src/app/(public)/cryaistal-agent/page.tsx
@@ -1,16 +1,26 @@
 import type { CHARACTERS } from "@/config";
 import { getInitialMessage } from "@/utils/chat";
+import { redirect } from "next/navigation";
+import { Suspense } from "react";
 import Container from "./_components/container";
 
 interface IPageProps {
   searchParams: Promise<{
     type: string;
     walletAddress: string;
+    wallet:string;
   }>;
 }
 
 export default async function Page({ searchParams }: IPageProps) {
-  const { type, walletAddress } = await searchParams;
+  const { type, walletAddress, wallet } = await searchParams;
+
+  if (!type) {
+    const defaultType = "GOOD";
+    return redirect(
+      `/cryaistal-agent?type=${defaultType}${walletAddress ? `&walletAddress=${walletAddress}` : ""}${wallet ? `&wallet=${wallet}` : ""}`,
+    );
+  }
 
   const firstAskQuestionPromise = getInitialMessage({
     character: type,
@@ -18,12 +28,20 @@ export default async function Page({ searchParams }: IPageProps) {
   });
 
   return (
-    // <Suspense key={walletAddress} fallback={<LoadingStatic />}>
-    <Container
-      type={type as (typeof CHARACTERS)[number]["id"]}
-      walletAddress={walletAddress}
-      firstAskQuestionPromise={firstAskQuestionPromise}
-    />
-    // </Suspense>
+    <Suspense
+      key={walletAddress}
+      fallback={
+        <div className="flex min-h-screen items-center justify-center text-white">
+          Loading...
+        </div>
+      }
+    >
+      <Container
+        type={type as (typeof CHARACTERS)[number]["id"]}
+        walletAddress={walletAddress}
+        wallet={wallet}
+        firstAskQuestionPromise={firstAskQuestionPromise}
+      />
+    </Suspense>
   );
 }
