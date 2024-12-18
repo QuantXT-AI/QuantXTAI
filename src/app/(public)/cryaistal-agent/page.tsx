130530c9506140106917642a14bf775c8a9d0c5c
QuantXT-AI <maximus42421@mail.com>
2024-12-18 15:45:24 +0700

    fix

--- src/app/(public)/cryaistal-agent/page.tsx
+++ src/app/(public)/cryaistal-agent/page.tsx
@@ -12,11 +12,6 @@ interface IPageProps {
 export default async function Page({ searchParams }: IPageProps) {
   const { type, walletAddress } = await searchParams;
 
-  if (!type) {
-    const defaultType = "GOOD";
-    return (window.location.href = `/cryaistal-agent?type=${defaultType}${walletAddress ? `&walletAddress=${walletAddress}` : ""}`);
-  }
-
   const firstAskQuestionPromise = getInitialMessage({
     character: type,
     walletAddress,
