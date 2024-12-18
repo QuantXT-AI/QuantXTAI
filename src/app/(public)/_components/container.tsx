ea0a9c4bf89db621e765667e68e899b2894ff91f
QuantXT-AI <maximus42421@mail.com>
2024-12-18 20:59:18 +0700

    fix

--- src/app/(public)/_components/container.tsx
+++ src/app/(public)/_components/container.tsx
@@ -2,7 +2,6 @@
 
 import dynamic from "next/dynamic";
 
-import Section4 from "./section-4";
 import Section5 from "./section-5";
 import Section7 from "./section-7";
 import Section8 from "./section-8";
@@ -13,6 +12,10 @@ const Section1 = dynamic(() => import("./section-1"), {
   ssr: false,
 });
 const Section6 = dynamic(() => import("./section-6"), {
+  ssr: false,
+});
+
+const Section4 = dynamic(() => import("./section-4"), {
   ssr: false,
 });
 
